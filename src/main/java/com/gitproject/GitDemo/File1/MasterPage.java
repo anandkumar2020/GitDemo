49be1965f074047cfc91646e2d21742b37af2c0e
Anand Kumar <76617523+anandkumar2020@users.noreply.github.com>
2021-11-07 19:50:50 +0530

    Master File

---
+++ src/main/java/com/gitproject/GitDemo/File1/MasterPage.java
@@ -0,0 +1,13 @@
+package com.gitproject.GitDemo.File1;
+
+public class MasterPage {
+
+
+            public void Mymaster(){
+
+
+                System.out.println("Hello Master");
+
+            }
+
+}
