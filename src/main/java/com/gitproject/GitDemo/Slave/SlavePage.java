9d0c7ac4b18ad04be32bd7726f8e4070b36cdc90
Anand Kumar <76617523+anandkumar2020@users.noreply.github.com>
2021-11-07 20:45:56 +0530

    Slave chnage

---
+++ src/main/java/com/gitproject/GitDemo/Slave/SlavePage.java
@@ -0,0 +1,16 @@
+package com.gitproject.GitDemo.Slave;
+
+public class SlavePage {
+
+
+
+                public void mymaster(){
+
+
+                    System.out.println("Hello Slave");
+                    
+
+                }
+
+
+}
